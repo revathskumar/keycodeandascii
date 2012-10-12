5fbf39a8ce54e130fc09f86f94445fed2e4a4444
Revath S Kumar <rsk@revathskumar.com>
2012-10-13 02:42:09 +0530

    removed navbar from index and refactored main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
+jQuery(function($){
+  last_keys = [];
+  $(document)
+    .keypress(getAscii)
+    .keyup(getKeyCode);
+});
 
+function getAscii (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  //ascii
+  char = String.fromCharCode(e.which);
+  $("#character-container").html(char);
+  $("#ascii-container").html(e.which);
+  return false;
+}
+
+function getKeyCode(e){
+  e.preventDefault();
+  e.stopPropagation();
+
+  code = e.which.toString();
+  if(e.shiftKey){
+    last_keys.push(code);
+  }
+  else{
+    last_keys = [];
+  }
+  if(last_keys.length == 3){
+    last_keys.shift();
+  }
+  $("#keycode-container").html(code);
+  keys = {16: "Shift",9: "Tab", 13: "Enter", 8: "Backspace", }
+  $("#keycode-container").html(code);
+  if(keys.hasOwnProperty(code) && last_keys[0] != 16){
+    $("#character-container").html(keys[code]);
+    $("#ascii-container").html("");
+  }
+  return false;
+}
